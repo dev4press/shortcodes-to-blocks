892150f8b8e9749cd4c198382cde08d1f709ccc3
Milan Petrovic <milan@gdragon.info>
2021-04-30 11:46:27 +0200

    Few changes.

--- src/press-meta/index.js
+++ src/press-meta/index.js
@@ -1,15 +1,15 @@
 import {registerBlockType} from '@wordpress/blocks';
 import ServerSideRender from '@wordpress/server-side-render';
-import {useBlockProps} from "@wordpress/block-editor";
-import {PanelBody, TextControl, ToggleControl} from "@wordpress/components";
-import {InspectorControls} from "@wordpress/editor";
-import {__} from "@wordpress/i18n";
+import {useBlockProps} from '@wordpress/block-editor';
+import {PanelBody, TextControl, ToggleControl} from '@wordpress/components';
+import {InspectorControls} from '@wordpress/editor';
+import {__} from '@wordpress/i18n';
 
 registerBlockType('shortcodes-to-blocks/press-meta', {
     apiVersion: 2,
     name: 'shortcodes-to-blocks/press-meta',
-    title: __("Press Meta", "shortcodes-to-blocks"),
-    description: __("Display simple meta links.", "shortcodes-to-blocks"),
+    title: __('Press Meta', 'shortcodes-to-blocks'),
+    description: __('Display simple meta links.', 'shortcodes-to-blocks'),
     icon: 'menu-alt',
     attributes: {
         'title': {
@@ -60,62 +60,62 @@ registerBlockType('shortcodes-to-blocks/press-meta', {
         return (
             <div {...useBlockProps()}>
                 <ServerSideRender
-                    block="shortcodes-to-blocks/press-meta"
+                    block='shortcodes-to-blocks/press-meta'
                     attributes={attributes}
                 />
-                <InspectorControls key="settings">
-                    <PanelBody title={__("Display", "shortcodes-to-blocks")}>
+                <InspectorControls key='settings'>
+                    <PanelBody title={__('Display', 'shortcodes-to-blocks')}>
                         <TextControl
-                            label={__("Title", "shortcodes-to-blocks")}
+                            label={__('Title', 'shortcodes-to-blocks')}
                             value={attributes.title}
                             onChange={(value) => setAttributes({title: value})}
                         />
                     </PanelBody>
-                    <PanelBody title={__("Links", "shortcodes-to-blocks")}>
+                    <PanelBody title={__('Links', 'shortcodes-to-blocks')}>
                         <ToggleControl
-                            label={__("Show Site Admin", "shortcodes-to-blocks")}
+                            label={__('Show Site Admin', 'shortcodes-to-blocks')}
                             checked={attributes.showSiteAdmin}
                             onChange={(value) => setAttributes({showSiteAdmin: value})}
                         />
                         <ToggleControl
-                            label={__("Show Profile Edit", "shortcodes-to-blocks")}
+                            label={__('Show Profile Edit', 'shortcodes-to-blocks')}
                             checked={attributes.showProfileEdit}
                             onChange={(value) => setAttributes({showProfileEdit: value})}
                         />
                         <ToggleControl
-                            label={__("Show Log In", "shortcodes-to-blocks")}
+                            label={__('Show Log In', 'shortcodes-to-blocks')}
                             checked={attributes.showLogIn}
                             onChange={(value) => setAttributes({showLogIn: value})}
                         />
                         <ToggleControl
-                            label={__("Show Log Out", "shortcodes-to-blocks")}
+                            label={__('Show Log Out', 'shortcodes-to-blocks')}
                             checked={attributes.showLogOut}
                             onChange={(value) => setAttributes({showLogOut: value})}
                         />
                         <ToggleControl
-                            label={__("Show Register", "shortcodes-to-blocks")}
+                            label={__('Show Register', 'shortcodes-to-blocks')}
                             checked={attributes.showRegister}
                             onChange={(value) => setAttributes({showRegister: value})}
                         />
                         <ToggleControl
-                            label={__("Show Home Page", "shortcodes-to-blocks")}
+                            label={__('Show Home Page', 'shortcodes-to-blocks')}
                             checked={attributes.showHomePage}
                             onChange={(value) => setAttributes({showHomePage: value})}
                         />
                         <ToggleControl
-                            label={__("Show RSS Posts", "shortcodes-to-blocks")}
+                            label={__('Show RSS Posts', 'shortcodes-to-blocks')}
                             checked={attributes.showRSSPosts}
                             onChange={(value) => setAttributes({showRSSPosts: value})}
                         />
                         <ToggleControl
-                            label={__("Show RSS Comments", "shortcodes-to-blocks")}
+                            label={__('Show RSS Comments', 'shortcodes-to-blocks')}
                             checked={attributes.showRSSComments}
                             onChange={(value) => setAttributes({showRSSComments: value})}
                         />
                     </PanelBody>
-                    <PanelBody title={__("Advanced", "shortcodes-to-blocks")}>
+                    <PanelBody title={__('Advanced', 'shortcodes-to-blocks')}>
                         <TextControl
-                            label={__("Additional CSS Class", "shortcodes-to-blocks")}
+                            label={__('Additional CSS Class', 'shortcodes-to-blocks')}
                             value={attributes.class}
                             onChange={(value) => setAttributes({class: value})}
                         />
